3ddcdb99b2f6c5333029d1a93dbeae7fc31f6f14
ab5sr <ab5sr@hotmail.com>
2020-03-27 15:26:48 -0500

    Add files via upload

---
+++ Exercise 4-3.sql
@@ -0,0 +1,22 @@
+--1
+--Write a SELECT statement to the Application.Countries table, creating
+--a new code, which is the first three characters of the CountryName capitalized
+--Alias the column NewCode, returning only the column created and the IsoAlpha3Code attribute.
+--Hint: you will use both the UPPER() function, and the LEFT() function.
+
+SELECT UPPER(LEFT(CountryName,3)) AS NewCode, IsoAlpha3Code
+FROM Application.Countries
+
+--2.
+--In the CustomerName (located in the Sales.Customer table) return only the portion
+--inside of parenthesis, including the parenthesis. Hint: See pp. 74 'Nesting Function',
+--You may need to use a number of built-in functions, such as SUBSTRING(), CHARINDEX() AND LEN().
+
+SELECT  SUBSTRING(customername, CHARINDEX('(', customername), LEN(customername))
+FROM [WideWorldImporters].[Sales].[Customers]
+
+--3.
+--Does TRIM() work the same as LTRIM(TRIM()), as was shown in the chapter?
+
+--Yes, the same results are achieved and you do not have to use 2 functions.
+
