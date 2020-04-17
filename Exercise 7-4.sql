760c125f12ca2286fd94ca6193908af77b123cd9
ab5sr <ab5sr@hotmail.com>
2020-04-17 16:53:13 -0500

    Add files via upload

---
+++ Exercise 7-4.sql
@@ -0,0 +1,18 @@
+--1.
+--Write a query that returns the DISTINCT LIST OF UnitPrice
+--from the Sales.OrderLines table
+--and Order the results by UnitPrice DESC
+--Return ONLY the UnitPrice.
+
+SELECT  DISTINCT UnitPrice
+FROM Sales.OrderLines
+ORDER BY UnitPrice DESC
+
+--2.
+--Write the same query in #1 but this time use a GROUP BY instead of DISTINCT.
+
+
+SELECT   UnitPrice
+FROM Sales.OrderLines
+GROUP BY UnitPrice
+ORDER BY UnitPrice DESC
