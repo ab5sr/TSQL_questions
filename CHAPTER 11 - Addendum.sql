22d704d55ba4370e8a6fbadc97c324509afe9109
ab5sr <ab5sr@hotmail.com>
2020-05-03 11:44:30 -0500

    Update CHAPTER 11 - Addendum.sql

--- CHAPTER 11 - Addendum.sql
+++ CHAPTER 11 - Addendum.sql
@@ -1,5 +1,7 @@
 --1.
 --Here is a simple CTE with CROSS APPLY
+--Here is another simple CTE with CROSS APPLY
+--This query simply compares LastEditedBy from two tables.
 --Note that this query can be rewritten using an INNER JOIN
 
 WITH Od
@@ -16,8 +18,7 @@ CROSS APPLY
 ORDER BY 1,2,3
 
 --2.
---Here is another simple CTE with CROSS APPLY
---This query simply compares LastEditedBy from two tables.
+--Here is a use of CROSS APPLY in combination with the STRING_SPLIT() internal table-returning function
 --Note that this query cannnot be rewritten using an INNER JOIN
 
 
